37137ecf524636a1ab39ff32c7468585cd448458
xagau <seanbeecroft@gmail.com>
2018-07-31 23:34:41 -0400

    Create PlaceholderMain.java

---
+++ core/src/main/java/com/openwallet/core/coins/PlaceholderMain.java
@@ -0,0 +1,34 @@
+package com.openwallet.core.coins;
+
+import com.openwallet.core.coins.families.PeerFamily;
+
+/**
+ * @author John L. Jegutanis
+ */
+public class PlaceholderMain extends PeerFamily {
+    private PlaceholderMain() {
+        id = "plach.main";
+
+        addressHeader = 35;
+        p2shHeader = 5;
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
+        spendableCoinbaseDepth = 100;
+        dumpedPrivateKeyHeader = 0xB0;
+
+        name = "Placeh";
+        symbol = "PHL";
+        uriScheme = "placeh";
+        bip44Index = 10;
+        unitExponent = 8;
+        feeValue = value(10000); // 0.0001 BLK
+        minNonDust = value(1);
+        softDustLimit = value(1000000); // 0.01 BLK
+        softDustPolicy = SoftDustPolicy.AT_LEAST_BASE_FEE_IF_SOFT_DUST_TXO_PRESENT;
+        signedMessageHeader = toBytes("BlackCoin Signed Message:\n");
+    }
+
+    private static PlaceholderMain instance = new PlaceholderMain();
+    public static synchronized CoinType get() {
+        return instance;
+    }
+}
